db6d589575fcadb555db72d137b8a47e7264891c
MHamdanHariri20 <m.hamdanhariri5@gmail.com>
2024-09-02 08:31:02 +0700

    revition

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -7,7 +7,7 @@ function Home(){
                     <div className="content-text">
                         <h1>Berkomitmen Menyediakan Fasilitas <span>Ibadah Premium</span> untuk Anda</h1>
                         <p>Tingkatkan kualitas ibadahmu dengan fasilitas premium kami yang akan membuat pengalaman ibadah Anda lebih tertata, menenangkan, penuh berkah, dan meningkatkan rasa syukur kepada Allah Swt.</p>
-                        <button>Baca Selengkapnya</button>
+                        <a href="#about"><button>Baca Selengkapnya</button></a>
                     </div>
 
                 </div>

88e5dd44237523959e137dc4ad7ba69a8d37f830
timmdkrtf <contact@mdkrtf.co.id>
2025-07-14 03:21:53 -0500

    revisi habatour

--- src/components/Testimoni.jsx
+++ src/components/Testimoni.jsx
@@ -1,32 +1,53 @@
+import React from "react";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css"; 
+import "slick-carousel/slick/slick-theme.css";
 
+export default function Testimoni() {
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    arrows: false,
+    autoplay: true,
+    autoplaySpeed: 3000,
+    slidesToShow: 2,
+    slidesToScroll: 1,
+  };
 
-export default function Testimoni(){
-    return(
-        <div className="testimoni">
-            <div className="container">
-                <div className="content-flex">
-                    <div className="title">
-                        <h4>Apa Kata Mereka?</h4>
-                    </div>
-                    <div className="content-quote">
-                        <div className="testi a">
-                            <p>
-                                Kegiatan sholat berjamaah wajib dan sunahny (buat sy ini sprti miniatur umroh) MaasyaaAllaah. do'akan sy dpt ikut kmbali dlm kegiatan yg diadakan tim ASW hingga Allaah panggil utk Umroh bahkan Haji 🤲🤲....Aamiin ya Allaah
-                            </p>
-                        </div>
-                        <div className="testi b">
-                            <p>
-                                Habatour harus tetap mengutamakan kenyamanan jamaah baik alat transportasi, hotel dan makanan. Terima kasih Habatour, sukses selalu dan semakin banyak jamaah yang puas dengan pelayanannya. Berkah..berkah..berkah...
-                            </p>
-                        </div>
-                        <div className="testi a">
-                            <p>
-                                Alhamdulillah bagus .mudah2an kedepannya lebih bagus lagi
-                            </p>
-                        </div>
-                    </div>
+  const testimonials = [
+    {
+      id: 1,
+      text: `Kegiatan sholat berjamaah wajib dan sunahny (buat sy ini sprti miniatur umroh) MaasyaaAllaah. do'akan sy dpt ikut kmbali dlm kegiatan yg diadakan tim ASW hingga Allaah panggil utk Umroh bahkan Haji 🤲🤲....Aamiin ya Allaah`
+    },
+    {
+      id: 2,
+      text: `Habatour harus tetap mengutamakan kenyamanan jamaah baik alat transportasi, hotel dan makanan. Terima kasih Habatour, sukses selalu dan semakin banyak jamaah yang puas dengan pelayanannya. Berkah..berkah..berkah...`
+    },
+    {
+      id: 3,
+      text: `Alhamdulillah bagus .mudah2an kedepannya lebih bagus lagi`
+    }
+  ];
+
+  return (
+    <div className="testimoni">
+      <div className="container">
+        <div className="content-flex">
+          <div className="title">
+            <h4>Apa Kata Mereka?</h4>
+          </div>
+          <div className="content-quote">
+            <Slider {...settings}>
+              {testimonials.map((item) => (
+                <div key={item.id} className="testi">
+                  <p>{item.text}</p>
                 </div>
-            </div>
+              ))}
+            </Slider>
+          </div>
         </div>
-    );
+      </div>
+    </div>
+  );
 }

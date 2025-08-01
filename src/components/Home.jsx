da4c90ca63236f8908fb31de1b278e8d935fbd74
timmdkrtf <contact@mdkrtf.co.id>
2025-08-01 03:23:13 -0500

    ganti style slide img yang sebelumnya pakai bg img

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -6,35 +6,48 @@ import view3 from "../assets/img/MDK09126.jpg"
 import view4 from "../assets/img/MDK09166.jpg"
 const images = [view3, view4, view1, view2];
 
-export default function Home(){
+export default function Home() {
+  const [currentImage, setCurrentImage] = useState(0);
+  const [fade, setFade] = useState(false);
 
-    const [currentImage, setCurrentImage] = useState(0);
-    const [fade, setFade] = useState(false);
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setFade(true);
+      setTimeout(() => {
+        setCurrentImage((prev) => (prev + 1) % images.length);
+        setFade(false);
+      }, 300); // fade duration
+    }, 6000);
 
-    useEffect(() => {
-        const interval = setInterval(() => {
-            setFade(true); 
-            setTimeout(() => {
-                setCurrentImage((prev) => (prev + 1) % images.length);
-                setFade(false);
-            }, 300); 
-        }, 6000); 
+    return () => clearInterval(interval);
+  }, []);
 
-        return () => clearInterval(interval);
-    }, []);
+  return (
+    <div id="home" className="home">
+      {/* Background Image Layer */}
+      <img
+        src={images[currentImage]}
+        alt="background"
+        className={`background-image ${fade ? "fade-out" : "fade-in"}`}
+      />
 
-    return(
-        <div id="home" className={`home ${fade ? "fade-out" : "fade-in"}`} style={{ backgroundImage: `url(${images[currentImage]})` }}>
-            <div className="container">
-                <div className="row">
-                    <div className="content">
-                        <h1>
-                            Jelajahi Wisata dengan Sentuhan Keindahan Islami
-                        </h1>
-                            <a className="btn explore" href="#about">Jelajahi Sekarang</a>
-                    </div>
-                </div>
-            </div>
+      {/* Overlay */}
+      <div className="overlay" />
+
+      {/* Content */}
+      <div className="container">
+        <div className="row">
+          <div className="content">
+            <h1>
+              Jelajahi Wisata Berkesan dengan Sentuhan{" "}
+              <span className="budaya-alam">Budaya Alam</span>
+            </h1>
+            <a className="btn explore" href="#about">
+              Jelajahi Sekarang
+            </a>
+          </div>
         </div>
-    );
+      </div>
+    </div>
+  );
 }
